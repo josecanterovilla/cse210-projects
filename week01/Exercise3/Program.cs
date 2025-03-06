70793ac51914fb1147fddfb27ff63bddd8a30a83
josecanterovilla <josevc@byui.edu>
2025-03-06 14:13:19 -0300

    a

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,13 +9,23 @@ class Program
         Random randomGenerator = new Random();
         int magic_number = randomGenerator.Next(1,100);
 
-        Console.Write("What is your gress?: ");
+        
+        int repeats = 0;
+        string continue_playing = "";
+
+        while (continue_playing != "no")
+
+        {Console.Write("What is your guess?: ");
         string inputUser2 = Console.ReadLine();
         int guess_number = int.Parse(inputUser2);
+        
+        repeats ++;
 
-        while (magic_number != guess_number)
-        {if (magic_number == guess_number)
-        {Console.WriteLine("You guessed it!");}
+        if (magic_number == guess_number) 
+        {Console.WriteLine($"You guessed it!, with {repeats} guesses");
+        repeats = 0;
+        Console.Write("Do you want to keep playing?: ");
+        continue_playing = Console.ReadLine();}
 
         else if (magic_number < guess_number)
         {Console.WriteLine("Lower");}
@@ -23,5 +33,7 @@ class Program
         else if (magic_number > guess_number)
         {Console.WriteLine("Higher");}
         }
+        
+        
     }
 }
