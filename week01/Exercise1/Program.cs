0d52cea0bfb8bf37ddc3b17c06f3c90d46631b36
josecanterovilla <josevc@byui.edu>
2026-09-03 11:08:05 -0400

    ok

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -10,6 +10,7 @@ class Program
         string first_name = Console.ReadLine();
         Console.Write("What is your last name bro?: ");
         string last_name = Console.ReadLine();
+
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }
