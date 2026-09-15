c23276ee92830aab553ae31372b8abd3dbe94ab4
josecanterovilla <josevc@byui.edu>
2026-09-15 13:23:37 -0300

    changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,10 +7,10 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your first name?: ");
-        string first_name = Console.ReadLine();
+        string first = Console.ReadLine();
         Console.Write("What is your last name?: ");
-        string last_name = Console.ReadLine();
+        string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
+        Console.WriteLine($"Your name is {last}, {first} {last}. ");
     }
 }
